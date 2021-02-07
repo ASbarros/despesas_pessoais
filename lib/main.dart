f5490baa381147f3dcfda5adae59a90cad169255
asbarros <anderson.barros0298@gmail.com>
2021-02-07 15:55:19 -0300

    mudando a cor primaria

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
       home: MyHomePage(),
     );
   }
