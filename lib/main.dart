a3c3798c371642311dbcc5274204687c95d385c2
asbarros <anderson.barros0298@gmail.com>
2021-02-06 21:53:42 -0300

    colocando a lista de transacoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,14 +14,50 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'novo tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'conta de luz',
+      value: 255.96,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        centerTitle: true,
+        actions: [IconButton(icon: Icon(Icons.add), onPressed: null)],
       ),
-      body: Center(
-        child: Text('Versão inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 100,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Container(
+            height: 500,
+            child: Card(
+              color: Colors.purple,
+              elevation: 5,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
       ),
     );
   }
