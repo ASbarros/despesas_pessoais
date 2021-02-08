891672305d522516cbce9671bae785095e9068db
asbarros <anderson.barros0298@gmail.com>
2021-02-08 17:07:57 -0300

    refatornado

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'components/chart.dart';
-import 'components/transaction_form.dart';
-import 'components/transaction_list.dart';
-import 'models/transaction.dart';
+import 'pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -46,131 +41,6 @@ class MyApp extends StatelessWidget {
         const Locale('pt', 'BR'),
       ],
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't0',
-      title: 'conta antiga',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 33)),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 2)),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 2)),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 4)),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 5)),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 4)),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 5)),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'conta de luz',
-      value: 255.96,
-      date: DateTime.now(),
-    ),
-  ];
-
-  List<Transaction> get _recentTransactions {
-    return _transactions
-        .where(
-            (tr) => tr.date.isAfter(DateTime.now().subtract(Duration(days: 7))))
-        .toList();
-  }
-
-  _addTransaction(String title, double value, DateTime date) {
-    final newTransaction = Transaction(
-      id: Random().nextDouble().toString(),
-      title: title,
-      value: value,
-      date: date,
-    );
-
-    setState(() {
-      _transactions.add(newTransaction);
-    });
-
-    Navigator.of(context).pop();
-  }
-
-  _removeTransaction(String id) {
-    setState(() {
-      _transactions.removeWhere((tr) => tr.id == id);
-    });
-  }
-
-  _openTransactionFormModal(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          return TransactionForm(onSubmit: _addTransaction);
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _openTransactionFormModal(context),
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Chart(recentTransactions: _recentTransactions),
-          Flexible(
-            child: TransactionList(
-                transactions: _transactions, onRemove: _removeTransaction),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openTransactionFormModal(context),
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,96 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import '../components/chart.dart';
+import '../components/transaction_form.dart';
+import '../components/transaction_list.dart';
+import '../database/database_helper.dart';
+import '../models/transaction.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    DatabaseHelper.instance.queryAllRows('expences').then((value) {
+      // ignore: avoid_function_literals_in_foreach_calls
+      value.forEach((element) {
+        setState(() {
+          _transactions.add(Transaction.fromMap(element));
+        });
+      });
+    }).catchError(print);
+  }
+
+  List<Transaction> get _recentTransactions {
+    return _transactions
+        .where(
+            (tr) => tr.date.isAfter(DateTime.now().subtract(Duration(days: 7))))
+        .toList();
+  }
+
+  _addTransaction(String title, double value, DateTime date) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: date,
+    );
+
+    DatabaseHelper.instance.insert(newTransaction, 'expences');
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(onSubmit: _addTransaction);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add, size: 30),
+            onPressed: () => _openTransactionFormModal(context),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Chart(recentTransactions: _recentTransactions),
+          Flexible(
+            child: TransactionList(
+                transactions: _transactions, onRemove: _removeTransaction),
+          ),
+        ],
+      ),
+    );
+  }
+}
