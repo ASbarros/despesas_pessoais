23f31c281201bea2631a1f0c25247660948404f5
asbarros <anderson.barros0298@gmail.com>
2021-02-08 17:26:30 -0300

    excluindo do database

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -56,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   _removeTransaction(int id) {
     setState(() {
       _transactions.removeWhere((tr) => tr.id == id);
+      DatabaseHelper.instance.delete(id, 'expences');
     });
   }
 
