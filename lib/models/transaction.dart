4205e580386db631f6ab4da76f87ad0b79b84fc2
asbarros <anderson.barros0298@gmail.com>
2021-02-08 17:23:13 -0300

    colocando o fromMap e extendendo do objeto base

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -2,18 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 
-class Transaction {
-  final String id;
+import 'object_base.dart';
+
+class Transaction extends ObjectBase {
+  final int id;
   final String title;
   final double value;
   final DateTime date;
 
   Transaction(
-      {@required this.id,
+      {this.id,
       @required this.title,
       @required this.value,
       @required this.date});
 
+  @override
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -24,4 +27,17 @@ class Transaction {
   }
 
   String toJson() => json.encode(toMap());
+  factory Transaction.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Transaction(
+      id: map['id'],
+      title: map['title'],
+      value: double.parse(map['value']),
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
+    );
+  }
+
+  factory Transaction.fromJson(String source) =>
+      Transaction.fromMap(json.decode(source));
 }
