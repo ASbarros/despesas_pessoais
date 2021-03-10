e41d91d2218af1dd8a7a822006c6844273c620d3
asbarros <anderson.barros0298@gmail.com>
2021-03-09 21:03:44 -0300

    criando os botoes

--- lib/pages/backup/backup_page.dart
+++ lib/pages/backup/backup_page.dart
@@ -1,24 +1,9 @@
-import 'package:financas_pessoais/repositorys/backup/backup_repository.dart';
 import 'package:flutter/material.dart';
 
-class BackupPage extends StatefulWidget {
-  @override
-  _BackupPageState createState() => _BackupPageState();
-}
+import '../../providers/backup_provider.dart';
 
-class _BackupPageState extends State<BackupPage> {
-  final _repository = BackupRepository();
-
-  bool done = false;
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      done = true;
-    });
-  }
-
+class BackupPage extends StatelessWidget {
+  final _backup = BackupProvider();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,89 +11,52 @@ class _BackupPageState extends State<BackupPage> {
         title: Text('Backup'),
       ),
       body: Center(
-        child: Visibility(
-          replacement: CircularProgressIndicator(),
-          visible: done,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                icon: Icon(Icons.upload_sharp),
-                label: Text('Backup'),
-                onPressed: () async {
-                  setState(() {
-                    done = false;
-                  });
-                  final res = await _repository.writeBackup();
-                  if (res['success']) {
-                    setState(() {
-                      done = true;
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      action: SnackBarAction(
-                        label: 'OK',
-                        onPressed: () {},
-                      ),
-                      content: Text('Criado com sucesso!'),
-                      duration: Duration(seconds: 5),
-                    ));
-                  } else {
-                    setState(() {
-                      done = true;
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.red,
-                      action: SnackBarAction(
-                        label: 'OK',
-                        onPressed: () {},
-                      ),
-                      content: Text('Falha ao criar! ${res['msg']}'),
-                      duration: Duration(seconds: 5),
-                    ));
-                  }
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton.icon(
-                label: Text('Restaurar'),
-                icon: Icon(Icons.file_download),
-                onPressed: () async {
-                  setState(() {
-                    done = false;
-                  });
-                  final res = await _repository.readBackup();
-                  if (res['success']) {
-                    setState(() {
-                      done = true;
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      action: SnackBarAction(
-                        label: 'OK',
-                        onPressed: () {},
-                      ),
-                      content: Text('Restaurado sem falhas!'),
-                      duration: Duration(seconds: 5),
-                    ));
-                  } else {
-                    setState(() {
-                      done = true;
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.red,
-                      action: SnackBarAction(
-                        label: 'OK',
-                        onPressed: () {},
-                      ),
-                      content: Text('Falha ao restaurar! ${res['msg']}'),
-                      duration: Duration(seconds: 5),
-                    ));
-                  }
-                },
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              icon: Icon(Icons.upload_sharp),
+              label: Text('Backup'),
+              onPressed: () async {
+                _backup.writeCounter();
+                /* ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  backgroundColor: Colors.red,
+                  action: SnackBarAction(
+                    label: 'OK',
+                    onPressed: _backupController.writeCounter,
+                  ),
+                  content: Text('Falha ao criar! '),
+                  duration: Duration(seconds: 5),
+                )); */
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              label: Text('Restaurar'),
+              icon: Icon(Icons.file_download),
+              onPressed: () {
+                /* ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  action: SnackBarAction(
+                    label: 'OK',
+                    onPressed: () {},
+                  ),
+                  content: Text('Restaurado sem falhas!'),
+                  duration: Duration(seconds: 5),
+                ));
+
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  backgroundColor: Colors.red,
+                  action: SnackBarAction(
+                    label: 'OK',
+                    onPressed: () {},
+                  ),
+                  content: Text('Falha ao restaurar! '),
+                  duration: Duration(seconds: 5),
+                )); */
+                _backup.readCounter(context);
+              },
+            ),
+          ],
         ),
       ),
     );
