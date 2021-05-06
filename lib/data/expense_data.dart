d96db528e1862858c8b25ab42581a91adde6bcbf
asbarros <anderson.barros0298@gmail.com>
2021-05-06 16:01:56 -0300

    colocando novos dados de teste

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -1,79 +1,648 @@
-import 'package:financas_pessoais/models/expenses_model.dart';
-
 class ExpenseData {
   static var items = [
-    ExpensesModel(
-            id: 1, title: 'title', value: 10, category: 1, date: DateTime.now())
-        .toMap(),
-    ExpensesModel(
-            id: 2,
-            title: 'title1',
-            value: 10,
-            category: 2,
-            date: DateTime.parse('2021-02-26'))
-        .toMap(),
-    ExpensesModel(
-            id: 3,
-            title: 'title2',
-            value: 120,
-            category: 3,
-            date: DateTime.parse('2021-02-27'))
-        .toMap(),
-    ExpensesModel(
-            id: 4,
-            title: 'title3',
-            value: 150,
-            category: 4,
-            date: DateTime.parse('2021-01-27'))
-        .toMap(),
-    ExpensesModel(
-            id: 5,
-            title: 'title4',
-            value: 300,
-            category: 5,
-            date: DateTime.parse('2021-03-27'))
-        .toMap(),
-    ExpensesModel(
-            id: 6,
-            title: 'title5',
-            value: 200,
-            category: 1,
-            date: DateTime.parse('2021-03-20'))
-        .toMap(),
-    ExpensesModel(
-            id: 7,
-            title: 'title6',
-            value: 60,
-            category: 2,
-            date: DateTime.parse('2021-02-15'))
-        .toMap(),
-    ExpensesModel(
-            id: 8,
-            title: 'title7',
-            value: 40,
-            category: 3,
-            date: DateTime.parse('2021-01-10'))
-        .toMap(),
-    ExpensesModel(
-            id: 9,
-            title: 'title8',
-            value: 50,
-            category: 4,
-            date: DateTime.parse('2021-04-01'))
-        .toMap(),
-    ExpensesModel(
-            id: 10,
-            title: 'gasolina',
-            value: 30,
-            category: 5,
-            date: DateTime.parse('2021-04-02'))
-        .toMap(),
-    ExpensesModel(
-            id: 10,
-            title: 'investimento em fundos',
-            value: 100,
-            category: 3,
-            date: DateTime.parse('2021-04-23'))
-        .toMap(),
+    {
+      'id': 93,
+      'title': 'Air fryer(1/3)',
+      'value': 100.0,
+      'date': 1620248535160,
+      'category': 4
+    },
+    {
+      'id': 5,
+      'title': 'Almoço',
+      'value': 143.0,
+      'date': 1612148400000,
+      'category': 1
+    },
+    {
+      'id': 6,
+      'title': 'Anuidade diferenciada Santander',
+      'value': 33.25,
+      'date': 1611802800000,
+      'category': 1
+    },
+    {
+      'id': 17,
+      'title': 'Banco Inter',
+      'value': 200.0,
+      'date': 1609815600000,
+      'category': 3
+    },
+    {
+      'id': 18,
+      'title': 'Bateria de moto',
+      'value': 148.73,
+      'date': 1614135600000,
+      'category': 2
+    },
+    {
+      'id': 19,
+      'title': 'Capacete San Marino',
+      'value': 130.11,
+      'date': 1611370800000,
+      'category': 4
+    },
+    {
+      'id': 20,
+      'title': 'Celular xiaomi redmi 9',
+      'value': 1885.98,
+      'date': 1615863600000,
+      'category': 4
+    },
+    {
+      'id': 7,
+      'title': 'Cerveja',
+      'value': 40.0,
+      'date': 1613185200000,
+      'category': 4
+    },
+    {
+      'id': 22,
+      'title': 'Cerveja',
+      'value': 60.0,
+      'date': 1614913200000,
+      'category': 4
+    },
+    {
+      'id': 23,
+      'title': 'Cerveja',
+      'value': 60.0,
+      'date': 1614999600000,
+      'category': 4
+    },
+    {
+      'id': 24,
+      'title': 'Cerveja',
+      'value': 3.0,
+      'date': 1614999600000,
+      'category': 4
+    },
+    {
+      'id': 25,
+      'title': 'Coisas pra casa',
+      'value': 16.0,
+      'date': 1613876400000,
+      'category': 1
+    },
+    {
+      'id': 26,
+      'title': 'Conta de luz',
+      'value': 70.1,
+      'date': 1614913200000,
+      'category': 1
+    },
+    {
+      'id': 70,
+      'title': 'Contagem da garrafa de economia',
+      'value': 222.18,
+      'date': 1617297614438,
+      'category': 5
+    },
+    {
+      'id': 27,
+      'title': 'Corte de cabelo',
+      'value': 19.0,
+      'date': 1614222000000,
+      'category': 4
+    },
+    {
+      'id': 28,
+      'title': 'Corte de cabelo',
+      'value': 10.0,
+      'date': 1615518000000,
+      'category': 4
+    },
+    {
+      'id': 3,
+      'title': 'Duas aulas de rua',
+      'value': 100.0,
+      'date': 1612753200000,
+      'category': 1
+    },
+    {
+      'id': 4,
+      'title': 'Duas aulas de rua',
+      'value': 90.0,
+      'date': 1611716400000,
+      'category': 1
+    },
+    {
+      'id': 30,
+      'title': 'Duas pasta de dente',
+      'value': 7.0,
+      'date': 1614999600000,
+      'category': 1
+    },
+    {
+      'id': 8,
+      'title': 'Emissão da LAD',
+      'value': 150.0,
+      'date': 1610593200000,
+      'category': 1
+    },
+    {
+      'id': 31,
+      'title': 'Exame de moto',
+      'value': 250.0,
+      'date': 1615518000000,
+      'category': 1
+    },
+    {
+      'id': 32,
+      'title': 'Fatura cartão Santander',
+      'value': 57.89,
+      'date': 1615258800000,
+      'category': 1
+    },
+    {
+      'id': 33,
+      'title': 'Foi e de ouvido computador',
+      'value': 99.9,
+      'date': 1613962800000,
+      'category': 4
+    },
+    {
+      'id': 34,
+      'title': 'Garrafa de economia',
+      'value': 0.1,
+      'date': 1613703600000,
+      'category': 5
+    },
+    {
+      'id': 35,
+      'title': 'Garrafa de economia',
+      'value': 0.1,
+      'date': 1614049200000,
+      'category': 5
+    },
+    {
+      'id': 36,
+      'title': 'Garrafa de economia',
+      'value': 0.5,
+      'date': 1614308400000,
+      'category': 5
+    },
+    {
+      'id': 37,
+      'title': 'Garrafa de economia',
+      'value': 1.5,
+      'date': 1614913200000,
+      'category': 5
+    },
+    {
+      'id': 38,
+      'title': 'Garrafa de economia',
+      'value': 2.0,
+      'date': 1615604400000,
+      'category': 5
+    },
+    {
+      'id': 39,
+      'title': 'Gasolina',
+      'value': 20.0,
+      'date': 1614049200000,
+      'category': 2
+    },
+    {
+      'id': 40,
+      'title': 'Internet',
+      'value': 69.9,
+      'date': 1613617200000,
+      'category': 1
+    },
+    {
+      'id': 41,
+      'title': 'Internet da roça',
+      'value': 100.0,
+      'date': 1615777200000,
+      'category': 1
+    },
+    {
+      'id': 42,
+      'title': 'Lanche',
+      'value': 16.0,
+      'date': 1613617200000,
+      'category': 4
+    },
+    {
+      'id': 43,
+      'title': 'Lanche',
+      'value': 45.5,
+      'date': 1614308400000,
+      'category': 4
+    },
+    {
+      'id': 44,
+      'title': 'Luz',
+      'value': 62.74,
+      'date': 1610420400000,
+      'category': 1
+    },
+    {
+      'id': 45,
+      'title': 'Luz',
+      'value': 47.47,
+      'date': 1610420400000,
+      'category': 4
+    },
+    {
+      'id': 46,
+      'title': 'Luz',
+      'value': 45.77,
+      'date': 1611802800000,
+      'category': 1
+    },
+    {
+      'id': 47,
+      'title': 'Luz',
+      'value': 45.44,
+      'date': 1611802800000,
+      'category': 1
+    },
+    {
+      'id': 48,
+      'title': 'Luz casa de cima',
+      'value': 88.52,
+      'date': 1614481200000,
+      'category': 1
+    },
+    {
+      'id': 49,
+      'title': 'Marcação exame direção moto',
+      'value': 250.0,
+      'date': 1612839600000,
+      'category': 1
+    },
+    {
+      'id': 50,
+      'title': 'Matrícula aula de rua',
+      'value': 410.0,
+      'date': 1607655600000,
+      'category': 1
+    },
+    {
+      'id': 9,
+      'title': 'Mercado pago (12/12)',
+      'value': 9.16,
+      'date': 1611630000000,
+      'category': 4
+    },
+    {
+      'id': 51,
+      'title': 'Merenda',
+      'value': 4.0,
+      'date': 1615518000000,
+      'category': 1
+    },
+    {
+      'id': 52,
+      'title': 'Merendas',
+      'value': 24.9,
+      'date': 1613703600000,
+      'category': 1
+    },
+    {
+      'id': 21,
+      'title': 'Mesa escritório',
+      'value': 940.0,
+      'date': 1616122800000,
+      'category': 1
+    },
+    {
+      'id': 53,
+      'title': 'Mão de obra',
+      'value': 20.0,
+      'date': 1615518000000,
+      'category': 2
+    },
+    {
+      'id': 10,
+      'title': 'Netflix',
+      'value': 21.9,
+      'date': 1610852400000,
+      'category': 4
+    },
+    {
+      'id': 11,
+      'title': 'Netflix',
+      'value': 21.9,
+      'date': 1613530800000,
+      'category': 4
+    },
+    {
+      'id': 12,
+      'title': 'Netflix',
+      'value': 21.9,
+      'date': 1615950000000,
+      'category': 4
+    },
+    {
+      'id': 61,
+      'title': 'Netflix',
+      'value': 21.9,
+      'date': 1615950000000,
+      'category': 4
+    },
+    {
+      'id': 29,
+      'title': 'PDVAT',
+      'value': 12.3,
+      'date': 1611630000000,
+      'category': 1
+    },
+    {
+      'id': 54,
+      'title': 'Pagamento conta telefone',
+      'value': 21.72,
+      'date': 1611284400000,
+      'category': 1
+    },
+    {
+      'id': 55,
+      'title': 'Pagamento imposto de moto',
+      'value': 91.14,
+      'date': 1611543600000,
+      'category': 2
+    },
+    {
+      'id': 13,
+      'title': 'Passagem de ônibus',
+      'value': 13.85,
+      'date': 1610593200000,
+      'category': 1
+    },
+    {
+      'id': 56,
+      'title': 'Pinga',
+      'value': 11.0,
+      'date': 1614999600000,
+      'category': 4
+    },
+    {
+      'id': 14,
+      'title': 'Produtos globo',
+      'value': 22.9,
+      'date': 1611630000000,
+      'category': 4
+    },
+    {
+      'id': 2,
+      'title': 'Quinze aulas de rua',
+      'value': 675.0,
+      'date': 1611889200000,
+      'category': 1
+    },
+    {
+      'id': 57,
+      'title': 'Tarifa pacote de serviços BB',
+      'value': 28.25,
+      'date': 1612494000000,
+      'category': 1
+    },
+    {
+      'id': 58,
+      'title': 'Tarifa pacote de serviços BB',
+      'value': 2.69,
+      'date': 1612494000000,
+      'category': 1
+    },
+    {
+      'id': 15,
+      'title': 'Tarifa pacote serviços BB',
+      'value': 28.25,
+      'date': 1609902000000,
+      'category': 1
+    },
+    {
+      'id': 16,
+      'title': 'Tarifa pacote serviços BB',
+      'value': 25.56,
+      'date': 1612753200000,
+      'category': 1
+    },
+    {
+      'id': 1,
+      'title': 'Uma aula de rua',
+      'value': 45.0,
+      'date': 1611802800000,
+      'category': 1
+    },
+    {
+      'id': 60,
+      'title': 'Viseira de capacete',
+      'value': 5.01,
+      'date': 1615518000000,
+      'category': 5
+    },
+    {
+      'id': 87,
+      'title': 'aula moto pista',
+      'value': 100.0,
+      'date': 1619471667118,
+      'category': 1
+    },
+    {
+      'id': 62,
+      'title': 'cadeira gamer (1/10)',
+      'value': 86.82,
+      'date': 1616468400000,
+      'category': 4
+    },
+    {
+      'id': 64,
+      'title': 'cerveja',
+      'value': 44.0,
+      'date': 1616881472735,
+      'category': 4
+    },
+    {
+      'id': 91,
+      'title': 'cerveja',
+      'value': 20.0,
+      'date': 1619901435820,
+      'category': 4
+    },
+    {
+      'id': 92,
+      'title': 'cerveja',
+      'value': 40.0,
+      'date': 1619915395116,
+      'category': 4
+    },
+    {
+      'id': 83,
+      'title': 'coisas pra casa',
+      'value': 29.8,
+      'date': 1618687325310,
+      'category': 1
+    },
+    {
+      'id': 79,
+      'title': 'cortador de unha',
+      'value': 10.0,
+      'date': 1618262436619,
+      'category': 1
+    },
+    {
+      'id': 89,
+      'title': 'corte de cabelo',
+      'value': 12.0,
+      'date': 1619821704535,
+      'category': 4
+    },
+    {
+      'id': 75,
+      'title': 'curso de inglês udemy',
+      'value': 27.9,
+      'date': 1617764400000,
+      'category': 1
+    },
+    {
+      'id': 82,
+      'title': 'dentista',
+      'value': 500.0,
+      'date': 1618363655740,
+      'category': 1
+    },
+    {
+      'id': 69,
+      'title': 'dez pães',
+      'value': 5.0,
+      'date': 1616946275565,
+      'category': 1
+    },
+    {
+      'id': 85,
+      'title': 'dia de pizza',
+      'value': 54.0,
+      'date': 1619287400622,
+      'category': 4
+    },
+    {
+      'id': 72,
+      'title': 'dois todinhos',
+      'value': 1.5,
+      'date': 1617312797564,
+      'category': 4
+    },
+    {
+      'id': 66,
+      'title': 'garrafa de economia',
+      'value': 3.0,
+      'date': 1616881500336,
+      'category': 5
+    },
+    {
+      'id': 68,
+      'title': 'garrafa de economia',
+      'value': 1.5,
+      'date': 1616924716272,
+      'category': 5
+    },
+    {
+      'id': 71,
+      'title': 'garrafa de economia',
+      'value': 2.0,
+      'date': 1617298769215,
+      'category': 5
+    },
+    {
+      'id': 81,
+      'title': 'garrafa de economia',
+      'value': 7.7,
+      'date': 1618262547870,
+      'category': 5
+    },
+    {
+      'id': 86,
+      'title': 'garrafa de economia',
+      'value': 2.3,
+      'date': 1619288437774,
+      'category': 5
+    },
+    {
+      'id': 63,
+      'title': 'gasolina',
+      'value': 20.0,
+      'date': 1616881430524,
+      'category': 2
+    },
+    {
+      'id': 73,
+      'title': 'gasolina',
+      'value': 20.0,
+      'date': 1617332400000,
+      'category': 2
+    },
+    {
+      'id': 80,
+      'title': 'gasolina',
+      'value': 30.0,
+      'date': 1618262452733,
+      'category': 2
+    },
+    {
+      'id': 90,
+      'title': 'gasolina',
+      'value': 30.0,
+      'date': 1619821717162,
+      'category': 2
+    },
+    {
+      'id': 88,
+      'title': 'internet da roça',
+      'value': 100.0,
+      'date': 1618455600000,
+      'category': 1
+    },
+    {
+      'id': 77,
+      'title': 'internet da rua',
+      'value': 69.9,
+      'date': 1615777200000,
+      'category': 1
+    },
+    {
+      'id': 78,
+      'title': 'internet da rua',
+      'value': 69.9,
+      'date': 1618196400000,
+      'category': 1
+    },
+    {
+      'id': 84,
+      'title': 'investimentos em fundo ',
+      'value': 100.0,
+      'date': 1618851751889,
+      'category': 3
+    },
+    {
+      'id': 74,
+      'title': 'lanche',
+      'value': 15.0,
+      'date': 1617587342998,
+      'category': 4
+    },
+    {
+      'id': 65,
+      'title': 'petiscos',
+      'value': 8.5,
+      'date': 1616881485478,
+      'category': 4
+    },
+    {
+      'id': 67,
+      'title': 'pizza',
+      'value': 43.0,
+      'date': 1616897661057,
+      'category': 4
+    },
+    {
+      'id': 59,
+      'title': 'Óleo Mobil',
+      'value': 21.45,
+      'date': 1615518000000,
+      'category': 2
+    }
   ];
 }
