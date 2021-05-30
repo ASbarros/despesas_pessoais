c78401ebd6599e4184044fdb1dd7ddbfc60ee897
asbarros <anderson.barros0298@gmail.com>
2021-05-30 13:17:51 -0300

    tirando o category provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 import 'pages/category/category_page.dart';
 import 'pages/charts/charts.dart';
 import 'pages/home_page.dart';
-import 'providers/category_provider_.dart';
 import 'providers/chart_page_provider.dart';
 import 'providers/chart_pie_provider.dart';
 import 'providers/chart_line_provider.dart';
@@ -20,10 +19,6 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => CategoryProvider(),
-          lazy: true,
-        ),
         ChangeNotifierProvider(
           create: (_) => ChartPieProvider(),
           lazy: false,
